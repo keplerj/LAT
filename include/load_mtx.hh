b25f5ac1302606d4e1ffacbe9751ffbf7b946711
keplerj <johakepl@gmail.com>
2019-10-25 13:49:00 +0300

    Update load_mtx.hh

--- include/load_mtx.hh
+++ include/load_mtx.hh
@@ -1,6 +1,6 @@
 // "load_mtx.hh" -- implements template functions for reading and writing .mtx files as part of the L(inear) A(rrangement) T(oolbox) library.
 //
-// Copyright (C) 2019 Georgios N Printezis
+// Copyright (C) 2018 - 2019 Georgios N Printezis
 //
 // This file is part of the LAT library. This library is free
 // software; you can distribute it and/or modify it under the
